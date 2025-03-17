dbb58f09710bb549fc61a428f0e2693d2be10342
Rudi Butarbutar <rubutar@gmail.com>
2025-03-17 20:22:23 +0700

    initial commit

---
+++ FinSight/FinSight/FinSightApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinSightApp.swift
+//  FinSight
+//
+//  Created by Rudi Butarbutar on 17/03/25.
+//
+
+import SwiftUI
+
+@main
+struct FinSightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
